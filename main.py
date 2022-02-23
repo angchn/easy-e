8d915b6cc71e7d1a3f3b25898b73fec399027480
angchn <angelaisuychen@gmail.com>
2022-02-24 11:45:55 +1300

    created databse and html templates

--- main.py
+++ main.py
@@ -2,5 +2,15 @@ from flask import Flask, render_template, abort
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask (__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///pizza.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///student.db"
 db = SQLAlchemy(app)
+
+
+
+
+@app.route("/")
+def home():
+    return render_template ("home.html", page_title="Home")
+
+if __name__ == "__main__":
+    app.run(debug=True)
