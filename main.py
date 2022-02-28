2ea4209bf0c216cac45ebd1ef7b70bef90384fc0
angchn <angelaisuychen@gmail.com>
2022-02-28 14:39:30 +1300

    created template inheritance

--- main.py
+++ main.py
@@ -5,15 +5,6 @@ app = Flask (__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///student.db"
 db = SQLAlchemy(app)
 
-class user(db.model):
-    __tablename__ = "user_login"
-    id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String())
-    user_password = db.Column(db.String())
-    todo_id = db.Column(db.Integer, foreign_key=True)
-    calender_id = db.Column(db.Integer, foreign_key=True)
-    notes_id = db.Column(db.Integer, foreign_key=True)
-
 
 @app.route("/")
 def home():
