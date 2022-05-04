88e5f0a36a389470c35ebe7408d36afe868ea62f
angchn <angelaisuychen@gmail.com>
2022-05-05 11:45:14 +1200

    user login

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
 from re import I
 from flask import Flask, render_template, abort, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
 from config import Config
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 app = Flask (__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
 import models
-
-class LoginForm(FlaskForm):
-    username = StringField("username", validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField("password", validators=[InputRequired(), Length(min=8, max=80)])
-    remember = BooleanField("remember me")
-
-class RegisterForm(FlaskForm):
-    email = StringField("email", validators=[InputRequired(), Email(message="Invalid email"), Length(max=50)])
-    username = StringField("username", validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField("password", validators=[InputRequired(), Length(min=8, max=80)])
+from forms import LoginForm, RegisterForm
 
 @login_manager.user_loader
 def load_user(user_id):
